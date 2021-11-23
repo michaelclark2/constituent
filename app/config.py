058c75e49aee3cba093ff3252516172813ac19a0
Michael Clark <michael.clark1992@gmail.com>
2021-11-23 13:22:09 -0600

    installed flask_awscognito

---
+++ app/config.py
@@ -0,0 +1,9 @@
+import os
+
+class Config:
+    AWS_DEFAULT_REGION = "us-east-1"
+    AWS_COGNITO_DOMAIN = "localhost"
+    AWS_COGNITO_USER_POOL_ID = os.environ.get("AWS_COGNITO_USER_POOL_ID")
+    AWS_COGNITO_USER_POOL_CLIENT_ID = os.environ.get("AWS_COGNITO_USER_POOL_CLIENT_ID")
+    AWS_COGNITO_USER_POOL_CLIENT_SECRET = os.environ.get("AWS_COGNITO_USER_POOL_CLIENT_SECRET")
+    AWS_COGNITO_REDIRECT_URL = "localhost:5000/redirect"
