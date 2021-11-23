217db71e48822581544b1457f27a0dc3b877ae62
Michael Clark <michael.clark1992@gmail.com>
2021-11-23 11:20:52 -0600

    Revert "Merge pull request #1 from michaelclark2/setup-django"
    This reverts commit 3102b5184322b6398dc583696508017bf030a589, reversing
    changes made to 7c1ba5ba368dfd85b5646835e2a5ba5527b4f9e7.

--- constituent/bills/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.ViewBills.as_view(), name='index')
-]
--- constituent/bills/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-from django.views import View
-
-class ViewBills(View):
-    
-    def get(self, request):
-        return HttpResponse('<h1>AYELMAO</h1>')
