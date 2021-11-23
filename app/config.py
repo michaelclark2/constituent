556eb4b647a9af3cdde0983ca153fb306b792adb
Michael Clark <michael.clark1992@gmail.com>
2021-11-23 16:43:13 -0600

    logout successful, will make open source contrib soon

--- app/config.py
+++ app/config.py
@@ -7,3 +7,4 @@ class Config:
     AWS_COGNITO_USER_POOL_CLIENT_ID = os.environ.get("AWS_COGNITO_USER_POOL_CLIENT_ID")
     AWS_COGNITO_USER_POOL_CLIENT_SECRET = os.environ.get("AWS_COGNITO_USER_POOL_CLIENT_SECRET")
     AWS_COGNITO_REDIRECT_URL = "http://localhost:5000/redirect"
+    AWS_COGNITO_SIGNOUT_URL = "http://localhost:5000/signout" 
--- app/routes/auth.py
+++ app/routes/auth.py
@@ -12,6 +12,14 @@ def aws_redirect():
     access_token = aws_auth.get_access_token(request.args)
     return jsonify({"access_token": access_token})
 
+@app.route('/logout')
+def logout():
+    return redirect(aws_auth.get_sign_out_url())
+
+@app.route('/signout')
+def signout():
+    return "You have signed out"
+
 
 @app.route('/auth_test')
 @aws_auth.authentication_required
