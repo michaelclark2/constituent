5f6d1407db015c465873d7e0e789c94afb966cef
Michael Clark <michael.clark1992@gmail.com>
2021-11-30 11:34:05 -0600

    added node-sass

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,9 +1,12 @@
-import './Header.css';
+import './Header.scss';
 
 const Header = (props) => {
     return (
         <header className="App-header">
             <h1>Constituent</h1>
+            <div className="header-btns">
+                <button className="btn btn-primary">Login</button>
+            </div>
         </header>
     )
 };
