db1df960bd66c12808e3d5fed3c728f7b2eb8acf
Michael Clark <michael.clark1992@gmail.com>
2021-11-30 11:02:47 -0600

    create-react-app, moved some files around

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import './Header.css';
+
+const Header = (props) => {
+    return (
+        <header className="App-header">
+            <h1>Constituent</h1>
+        </header>
+    )
+};
+
+export default Header;
