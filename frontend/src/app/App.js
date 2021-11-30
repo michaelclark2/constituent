c066f8a9913d990ce8f2fb3e29f28c0ab51e082a
Michael Clark <michael.clark1992@gmail.com>
2021-11-30 11:49:09 -0600

    prettier

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,5 +1,5 @@
-import Header from '../components/Header/Header';
-import './App.scss';
+import Header from "../components/Header/Header";
+import "./App.scss";
 
 function App() {
   return (
--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,14 +1,14 @@
-import './Header.scss';
+import "./Header.scss";
 
 const Header = (props) => {
-    return (
-        <header className="App-header">
-            <h1>Constituent</h1>
-            <div className="header-btns">
-                <button className="btn btn-primary">Login</button>
-            </div>
-        </header>
-    )
+  return (
+    <header className="App-header">
+      <h1>Constituent</h1>
+      <div className="header-btns">
+        <button className="btn btn-primary">Login</button>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
