6f3490be2e02046c0564a5054b00d5c0141bd6a8
Michael Clark <michael.clark1992@gmail.com>
2022-01-11 10:56:10 -0600

    upon user signup, lambda will create item in users table

--- backend/services/sorting-hat/handler.py
+++ backend/services/sorting-hat/handler.py
@@ -1,2 +1,12 @@
+import boto3
+
+dynamodb_client = boto3.client('dynamodb')
+
 def lambda_handler(event, context):
+
+    response = dynamodb_client.put_item(
+        TableName="users",
+        Item={"uid": {"S": event.get('userName')}}
+    )
+    print(response)
     return event
