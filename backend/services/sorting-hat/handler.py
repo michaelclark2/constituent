2e87f322119b71ee79ebbb3f0fa507297bd561a7
Michael Clark <michael.clark1992@gmail.com>
2022-03-22 14:21:01 -0500

    added state attribute to user

--- backend/services/sorting-hat/handler.py
+++ backend/services/sorting-hat/handler.py
@@ -3,10 +3,17 @@ import boto3
 dynamodb_client = boto3.client('dynamodb')
 
 def lambda_handler(event, context):
+    print(event)
 
+    new_user = event.get('request').get('userAttributes')
     response = dynamodb_client.put_item(
-        TableName="users",
-        Item={"uid": {"S": event.get('userName')}}
+        TableName="constituent.users",
+        Item={
+            "uid": {"S": event.get('userName')}, 
+            "username": {"S": new_user.get('email')},
+            "state": {"S": new_user.get('custom:state')},
+            "district": {"N": "0"},
+        }
     )
     print(response)
     return event
