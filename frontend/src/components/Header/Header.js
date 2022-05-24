7b894f4b5c82fd8517ff16480cd7497637cbff3a
Michael Clark <michael.clark1992@gmail.com>
2022-05-24 11:56:06 -0500

    better navbar

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -2,9 +2,41 @@ import "./Header.scss";
 
 const Header = (props) => {
   return (
-    <header className="App-header">
-      <h1>Constituent</h1>
-    </header>
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <div class="container-fluid">
+        <a class="navbar-brand text-primary h1 mb-0" href="#">
+          Constituent
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" href="#">
+                Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Bills
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link">Votes</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
